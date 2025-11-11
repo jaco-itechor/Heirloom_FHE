536da4fb79d4c3f1392fdcc0ab4fae7719aac8fd
jaco-itechor <jaco-itechor@users.noreply.github.com>
2025-11-11 12:38:38 +0000

    Add sms-mock-gateway.rs, simple-manager.json, src-sms.styl, window.d.ts

---
+++ frontend/web/src/service-gateway/sms-mock-gateway.rs
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/heirloom_fhe/frontend/web/src/service-gateway/sms-mock-gateway.rs
+// Generated at: 2025-11-27T12:25:21.274Z
+
+
